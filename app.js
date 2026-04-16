fced559b008836c6156ba7fd7b5c708ef58d5ae4
Nitai <nitai@helpmonks.com>
2026-04-16 17:17:45 -0400

    default process env

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ import path from 'node:path';
 import fs from 'node:fs';
 import { fileURLToPath } from 'node:url';
 
+require('./modules/process_env');
 import config from './config.js';
 import { connectDB } from './db.js';
 import { setupSocketIO } from './modules/socket.js';
